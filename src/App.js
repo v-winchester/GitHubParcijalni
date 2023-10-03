b6a2545a8666cf75c462a845ddbf122c97146359
V Winchester <v.miliceviic@hotmail.com>
2023-10-03 20:32:18 +0200

    -- first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from 'react';
 
-function App() {
+const App = () => {
+  const [username, setData] = useState({username: []});
+  const [isLoading, setIsLoading] = useState(false);
+  const [err, setErr] = useState('');
+  
+
+  const handleClick = async () => {
+    setIsLoading(true);
+
+    try {
+      
+
+      // -- pass user input to url??
+      const response = await fetch('https://api.github.com/users/'+{username}, {
+        method: 'GET',
+        headers: {
+          Accept: 'application/json',
+        },
+      });
+
+      if (!response.ok) {
+        throw new Error(`Error! status: ${response.status}`);
+      }
+
+      const result = await response.json();
+
+      setData(result);
+    } catch (err) {
+      setErr(err.message);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{textAlign: "center"}}>
+
+      <form />
+      <label for="username">GitHub username:</label><br/>
+      <input type="text" id="username" placeholder='e.g. facebook'/><br/>
+
+      <button onClick={handleClick}>Go !</button>
+
+      {isLoading && <h3>Loading...</h3>}
+      {err && <h2>{err}</h2>}
+
+      <div key={username.name}>
+            <li>{username.avatar_url}</li>
+            <li>{username.location}</li>
+            <li>{username.bio}</li>
+    </div>
     </div>
   );
-}
+};
 
 export default App;
